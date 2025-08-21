fd0b47624e18454f217e48835010cfa96b42b7fb
Zorana Lazarevic <zoranaism@Zoranas-MacBook-Pro-2.local>
2025-08-21 20:35:30 +0200

    updated hello title spacing

--- src/components/HeroBanner/PersonalPortfolio/index.js
+++ src/components/HeroBanner/PersonalPortfolio/index.js
@@ -7,7 +7,7 @@ const BannerPersonalPortfolio = () => {
     return (
         <HeroBannerContainer bg={bannerBg} overlay={false}>
             <div className="main-slider-content">
-                <h2>Hello , I’m</h2>
+                <h2>Hello, I’m</h2>
                 <h1>{personalInfo.name}</h1>
                 <h2>{personalInfo.designation}</h2>
                 {/* <a href={`${process.env.PUBLIC_URL + personalInfo.resumeURL}`}>Download Resume</a> */}
