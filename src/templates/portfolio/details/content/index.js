7fced413713bb8889d45908db0ab954b06a7a9ef
Zorana Lazarevic <zoranaism@Zoranas-MacBook-Pro-2.local>
2025-09-04 15:10:17 +0200

    Updated single pages portfolio data

--- src/templates/portfolio/details/content/index.js
+++ src/templates/portfolio/details/content/index.js
@@ -8,29 +8,33 @@ const PortfolioDetailsContent = ({title, slug, category, description, meta}) => 
             <h1 className="title">{title}</h1>
             <p>{description}</p>
             <ul className="project-info">
-                <li><span>Client:</span> {meta?.client}</li>
+                {/* <li><span>Client:</span> {meta?.client}</li>
                 <li><span>Date:</span> {meta?.date}</li>
                 <li><span>Website:</span>
                     <a href={meta?.website}
                        target="_blank"
                        rel="noopener noreferrer">{meta?.website}
                     </a>
-                </li>
+                </li> */}
                 <li>
                     <span>Tags:</span>
                     {meta?.services.map((service, index) => (
-                        <a key={index} href="/">{service}</a>
+                        <>
+                            <a  key={index} href="/">{service}</a>
+                            {index < meta.services.length - 1 && ", "}
+                        </>
                     ))}
+
                 </li>
             </ul>
 
-            <div className="project-share">
+            {/* <div className="project-share">
                 <span>Share:</span>
                 <a href="/"><i className="fa fa-facebook"/></a>
                 <a href="/"><i className="fa fa-twitter"/></a>
                 <a href="/"><i className="fa fa-pinterest-p"/></a>
                 <a href="/"><i className="fa fa-google-plus"/></a>
-            </div>
+            </div> */}
 
         </div>
     );
